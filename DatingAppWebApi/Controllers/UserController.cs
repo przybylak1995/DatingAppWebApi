4e45fecd7a4b6097d76d2e9cf62b41632e5f83c4
Kristof Przybylak <84039435+przybylak1995@users.noreply.github.com>
2023-02-17 11:36:09 +0100

    Add project files.

---
+++ DatingAppWebApi/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using DatingAppWebApi.Data;
+using DatingAppWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>>Getusers()
+        {
+           return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> Getusers(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ DatingAppWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingAppWebApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppWebApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
